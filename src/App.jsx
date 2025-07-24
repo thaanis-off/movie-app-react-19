51fdbdab29eeb9210ee190c89a78f98521919f31
thaanis-off <thaanisoff@gmail.com>
2025-07-25 02:45:49 +0530

    bismillah

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline' >
+        Hello world!
+      </h1>
+    </div>
+  )
+}
